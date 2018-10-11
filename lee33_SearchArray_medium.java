12cbedda427aec85db17f373ae05eb5a2ae39fd5
abcdddxy <kfc00m@126.com>
2018-10-11 15:55:12 +0800

    查找array

--- lee33_SearchArray_medium.java
+++ lee33_SearchArray_medium.java
@@ -4,10 +4,14 @@ public class lee33_SearchArray_medium {
 
         while (left <= right) {
             int mid = (left + right) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[left] <= target && nums[mid] >= target) right = mid - 1;
-            else if(nums[right] >= target && nums[mid] >= target) left = mid + 1;
-            else if(nums[right] <= target && nums[mid] <= target) left = mid + 1;
+            if (nums[mid] == target) return mid;
+            else if (nums[left] <= nums[mid]) {
+                if(nums[left] <= target && nums[mid] >= target) right = mid -1;
+                else left = mid + 1;
+            } else {
+                if(nums[mid] <= target && nums[right] >= target) left = mid + 1;
+                else right = mid - 1;
+            }
         }
 
         return -1;
