4c800bcf740dfdcd32b1deda5f340b565220a8fc
abcdddxy <kfc00m@126.com>
2018-10-23 21:53:50 +0800

    三角最短距离

--- lee120_Tria_medium.java
+++ lee120_Tria_medium.java
@@ -26,7 +26,10 @@ public class lee120_Tria_medium {
     public int minimumTotal2(List<List<Integer>> triangle) {
         int[] dp = new int[triangle.size() + 1];
         for (int i = triangle.size() - 1; i >= 0; i--) {
-
+            for (int j = 0; j < i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
         }
+        return dp[0];
     }
 }
