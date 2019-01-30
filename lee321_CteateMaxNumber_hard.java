241901e7d718c59fb7f3de5305b88c3d50a19dda
abcdddxy <kfc00m@126.com>
2019-01-30 20:27:37 +0800

    顺序不变最大数

--- lee321_CteateMaxNumber_hard.java
+++ lee321_CteateMaxNumber_hard.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class lee321_CteateMaxNumber_hard {
+    // 理解错题意了
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
         int n = nums1.length, m = nums2.length;
         int[] ans = new int[k], digits = new int[10];
@@ -36,17 +37,37 @@ public class lee321_CteateMaxNumber_hard {
         return ans;
     }
 
+    //dp
     public int[] maxNumber2(int[] nums1, int[] nums2, int k) {
-        for (int i = 0; i <= k; i++) {
-            int[] sub1 = findKMaxSub(nums1, i), sub2 = findKMaxSub(nums2, k - i);
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] ans = new int[k];
+        for (int i = Math.max(0, k - m); i <= k && i <= n; ++i) {
+            int[] candidate = merge(maxArray(nums1, i), maxArray(nums2, k - i), k);
+            if (greater(candidate, 0, ans, 0)) ans = candidate;
         }
+        return ans;
     }
-
-    public int[] findKMaxSub(int[] nums, int k) {
-        if (k == 0 || k > nums.length) return null;
-        else if (k == nums.length) return nums;
-        else {
-
+    private int[] merge(int[] nums1, int[] nums2, int k) {
+        int[] ans = new int[k];
+        for (int i = 0, j = 0, r = 0; r < k; ++r)
+            ans[r] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
+        return ans;
+    }
+    public boolean greater(int[] nums1, int i, int[] nums2, int j) {
+        while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
+            i++;
+            j++;
         }
+        return j == nums2.length || (i < nums1.length && nums1[i] > nums2[j]);
+    }
+    public int[] maxArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[k];
+        for (int i = 0, j = 0; i < n; ++i) {
+            while (n - i + j > k && j > 0 && ans[j - 1] < nums[i]) j--;
+            if (j < k) ans[j++] = nums[i];
+        }
+        return ans;
     }
 }
