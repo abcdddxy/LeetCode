575161a3fe5d349cd92372a6b61f0c9e8190ef93
abcdddxy <kfc00m@126.com>
2019-02-01 12:59:31 +0800

    好难的最大矩形

--- lee85_MaxRect_hard.java
+++ lee85_MaxRect_hard.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class lee85_MaxRect_hard {
+    //长宽法
     public int maximalRectangle(char[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) return 0;
         int h = matrix.length, w = matrix[0].length;
@@ -9,18 +10,61 @@ public class lee85_MaxRect_hard {
         Arrays.fill(right, w);
 
         for (int i = 0; i < h; i++) {
-            int leftBound = 0, rightBound = w + 1;
+            int leftBound = 0, rightBound = w;
             for (int j = 0; j < w; j++) {
                 if (matrix[i][j] == '1') {
                     height[j] += 1;
-                    left[j] = Math.max(left[j], leftBound);
-                    right[j] = Math.min(right[j], rightBound);
                 } else {
                     height[j] = 0;
+                }
+            }
+            for (int j = 0; j < w; j++) {
+                if (matrix[i][j] == '1') {
+                    left[j] = Math.max(left[j], leftBound);
+                } else {
+                    left[j] = 0;
                     leftBound = j + 1;
+                }
+            }
+            for (int j = w - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    right[j] = Math.min(right[j], rightBound);
+                } else {
+                    right[j] = w;
                     rightBound = j;
                 }
+            }
+            for (int j = 0; j < w; j++) {
                 res = Math.max((right[j] - left[j]) * height[j], res);
+            }
+        }
+        return res;
+    }
+
+    //计数法
+    public int maximalRectangle2(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return 0;
+        int h = matrix.length, w = matrix[0].length;
+        int res = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 1; j < w; j++) {
+                if (matrix[i][j] == '1') {
+                    matrix[i][j] = (char) (matrix[i][j-1] + 1);
+                } else {
+                    matrix[i][j] = '0';
+                }
+            }
+        }
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                int min = matrix[i][j] - '0';
+                if (min > 0) {
+                    res = Math.max(res, min);
+                    for (int k = i-1; k >= 0 && matrix[k][j] != '0'; k--) {
+                        min = Math.min(min, matrix[k][j] - '0');
+                        res = Math.max(res, min * (i-k+1));
+                    }
+                }
             }
         }
         return res;
