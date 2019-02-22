ae21d4ef84002c2b991256fbdc38a5b7bbf292bd
abcdddxy <kfc00m@126.com>
2019-02-22 21:27:28 +0800

    字符串回溯匹配

--- lee44_WildMatch_hard.java
+++ lee44_WildMatch_hard.java
@@ -1,26 +1,39 @@
 public class lee44_WildMatch_hard {
     // 递归超时
     public boolean isMatch(String s, String p) {
-        System.out.println("s = [" + s + "], p = [" + p + "]");
+        if (onlyContainAsterisk(p)) return true;
+        if (s.length() == 0 && p.length() == 0) return true;
+        if (s.length() == 0 || p.length() == 0) return false;
+        System.out.println("s = [" + replaceAsterisk(p) + "]");
+        return isWildMatch(s, replaceAsterisk(p));
+    }
+    private boolean isWildMatch(String s, String p){
         if (onlyContainAsterisk(p)) return true;
         if (s.length() == 0 && p.length() == 0) return true;
         if (s.length() == 0 || p.length() == 0) return false;
 
         switch (p.charAt(0)) {
             case '*':
-                return isMatch(s.substring(1), p) || isMatch(s, p.substring(1));
+                return isWildMatch(s.substring(1), p) || isWildMatch(s, p.substring(1));
             case '?':
-                return isMatch(s.substring(1), p.substring(1));
+                return isWildMatch(s.substring(1), p.substring(1));
             default:
-                return s.charAt(0) == p.charAt(0) && isMatch(s.substring(1), p.substring(1));
+                return s.charAt(0) == p.charAt(0) && isWildMatch(s.substring(1), p.substring(1));
         }
     }
-
     private boolean onlyContainAsterisk(String s) {
         if (s.length() == 0) return false;
         for (char c : s.toCharArray())
             if (c != '*') return false;
         return true;
+    }
+    private String replaceAsterisk(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (!(s.charAt(i - 1) == '*' && s.charAt(i) == '*')) sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 
     // 回溯
